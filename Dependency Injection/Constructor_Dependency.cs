d0f95c435ebdf4b99f0cb8de9baafde73d967052
“selva” <selvanathan@twilightsoftwares.com>
2023-11-16 11:41:11 +0530

    Add project files.

---
+++ Dependency Injection/Constructor_Dependency.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injection
+{
+   public interface IAccount
+    {
+        void PrintDetails();
+    }
+
+    public class CurrentAccount: IAccount
+    {
+        public void PrintDetails() 
+        {
+
+            Console.WriteLine("Details of Current Account");
+
+        }
+
+    }
+
+    public class SavingAccount: IAccount
+    {
+        public void PrintDetails()
+        {
+
+            Console.WriteLine("Details of Saving Account");
+
+        }
+
+    }
+
+    class Account
+    {
+        private IAccount account;
+
+        public Account(IAccount account)
+        {
+            this.account = account;
+        }
+
+        public void PrintAccounts()
+        {
+            account.PrintDetails();
+        }
+    }
+}
---
+++ Dependency Injection/Program.cs
@@ -0,0 +1,14 @@
+﻿using Dependency_Injection;
+
+class program
+{
+    static void Main( string [] args)
+    {
+        IAccount ca = new CurrentAccount();
+        Account a = new Account(ca);
+        a.PrintAccounts();
+
+       
+
+    }
+}
