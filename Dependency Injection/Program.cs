2a6bb1df01c3d304b13f8f15b8f3133c2c098ee7
“selva” <selvanathan@twilightsoftwares.com>
2023-11-16 12:14:40 +0530

    Property Dependency Injection

--- Dependency Injection/Program.cs
+++ Dependency Injection/Program.cs
@@ -4,6 +4,7 @@ class program
 {
     static void Main( string [] args)
     {
+        //Constructor Dependency Injection 
         IAccount ca = new CurrentAccount();
         Account a = new Account(ca);
         a.PrintAccounts();
@@ -12,12 +13,21 @@ class program
         Account a1 = new Account(sa);
         a1.PrintAccounts();
 
-
+        //Method Dependency Injection
         Account1 cam = new Account1();
         cam.PrintAccounts1(new CurrentAccount1());
 
         Account1 sam = new Account1();
         cam.PrintAccounts1(new SavingAccount1());
+
+        //Property Dependency Injection
+        Account2 cap = new Account2();
+        cap.account = new CurrentAccount2();
+        cap.PrintAcounts2();
+
+        Account2 sap = new Account2();
+        sap.account = new SavingAccount2();
+        sap.PrintAcounts2();
 
     }
 }
---
+++ Dependency Injection/Property_Dependency.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injection
+{
+    public interface IAccount2
+    {
+        void PrintDetails2();
+    }
+
+    public class CurrentAccount2 : IAccount2
+    {
+        public void PrintDetails2()
+        {
+
+            Console.WriteLine("Details of Current Account property");
+
+        }
+
+    }
+
+    public class SavingAccount2 : IAccount2
+    {
+        public void PrintDetails2()
+        {
+
+            Console.WriteLine("Details of Saving Account property ");
+
+        }
+
+    }
+
+    class Account2
+    {
+        public IAccount2 account { get; set; }
+
+        public void PrintAcounts2()
+        {
+            account.PrintDetails2();
+        }
+    }
+}
